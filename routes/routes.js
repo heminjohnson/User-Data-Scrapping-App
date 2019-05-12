99e1b0300fa2da6134c1aaf5cea1b2435a192f90
Hemin Johnson <achayan1992@gmail.com>
2019-05-12 15:02:12 +0200

    feat: get user image route should store the data in file system
    This router checks whether the userdata is already present in the file
    and if it does return that instead of sending an api request

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const axios = require('axios')
 const base64Arraybuffer = require('base64-arraybuffer')
+const R = require('ramda')
+const fs = require('fs')
 
 const router = new express.Router()
+
+const loadData = require('../services/loadData')
 
 router.get('/api/users/:id', async (req, res) => {
   const id = req.params.id
@@ -12,15 +16,30 @@ router.get('/api/users/:id', async (req, res) => {
 
 router.get('/api/users/:id/avatar', async (req, res) => {
   const id = req.params.id
-  const response = await axios.get(`https://reqres.in/api/users/${id}`)
 
-  const imageUrl = response.data.data.avatar
+  const userData = loadData('userData.json')
+  // This finds the index of the object from the array containing the user data based on the user id
+  const userObjectIndex = R.findIndex(R.propEq('id', id))(userData)
 
-  const imageBinary = await axios.get(imageUrl, {responseType: 'arraybuffer'})
-  const base64String = base64Arraybuffer.encode(imageBinary.data)
-  const base64StringCompleteData = `data:${imageBinary.headers['content-type']};base64,${base64String}`
+  if(userData === [] || userObjectIndex === -1) {
+    const response = await axios.get(`https://reqres.in/api/users/${id}`)
+    const imageUrl = response.data.data.avatar
 
-  res.send(base64StringCompleteData)
+    const imageBinary = await axios.get(imageUrl, {responseType: 'arraybuffer'})
+    const base64String = base64Arraybuffer.encode(imageBinary.data)
+    const base64StringCompleteData = `data:${imageBinary.headers['content-type']};base64,${base64String}`
+
+    //this pushes the data to userData.json file
+    userData.push({
+      id: req.params.id,
+      data: base64StringCompleteData
+    })
+    fs.writeFileSync('userData.json',JSON.stringify(userData))
+
+    res.send(base64StringCompleteData)
+  } else {
+    res.send(userData[userObjectIndex].data)
+  }
 })
 
 module.exports = router
