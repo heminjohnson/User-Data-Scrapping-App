d9166feb2189d0c47ae1b8322b141ebce7c62d39
Hemin Johnson <achayan1992@gmail.com>
2019-05-12 14:36:55 +0200

    feat: add the endpoint that returns the base64 image data

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const axios = require('axios')
+const base64Arraybuffer = require('base64-arraybuffer')
 
 const router = new express.Router()
 
@@ -7,6 +8,19 @@ router.get('/api/users/:id', async (req, res) => {
   const id = req.params.id
   const response = await axios.get(`https://reqres.in/api/users/${id}`)
   res.send(response.data)
+})
+
+router.get('/api/users/:id/avatar', async (req, res) => {
+  const id = req.params.id
+  const response = await axios.get(`https://reqres.in/api/users/${id}`)
+
+  const imageUrl = response.data.data.avatar
+
+  const imageBinary = await axios.get(imageUrl, {responseType: 'arraybuffer'})
+  const base64String = base64Arraybuffer.encode(imageBinary.data)
+  const base64StringCompleteData = `data:${imageBinary.headers['content-type']};base64,${base64String}`
+
+  res.send(base64StringCompleteData)
 })
 
 module.exports = router
